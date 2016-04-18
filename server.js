77d615d27a601fa38786cd695447f1034ba84e80
Sai Mamillapalli <Sai Mamillapalli>
2016-04-17 19:32:45 -0500

    Add gitignore attributes to node_modules

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 /**
  * Created by Sai on 4/3/2016.
  */
@@ -17,6 +15,12 @@ app.set("view engine", "handlebars");
 
 app.get("/", function(req,res){
    res.render("home", {title: "Hello from Webstorm" , message: "Hi there!"});
+});
+
+app.get("/api/getStatus", function(req,res){
+   res.status(200);
+   res.set("Content-type", "application/json");
+   res.send(["H1LCA","Bank","Utilities"]);
 });
 
 var server = http.createServer(app);
